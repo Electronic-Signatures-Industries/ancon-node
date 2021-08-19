3754a894553695a5411036fdc909860a0efbf5b0
block-mon <block.monkey.coder@gmail.com>
2021-08-19 05:35:50 -0500

    defined metadata type

--- internal/metadata/app/types.go
+++ internal/metadata/app/types.go
@@ -1,4 +1,13 @@
 package app
 
 // Metadata is the business model for metadata
-type Metadata struct{}
+type Metadata struct {
+	Name                []string
+	Description         []string
+	Image               []string
+	Sources             []string
+	Owner               []string
+	Parent              []string
+	VerifiedCredentials []string
+	Links               []string
+}
